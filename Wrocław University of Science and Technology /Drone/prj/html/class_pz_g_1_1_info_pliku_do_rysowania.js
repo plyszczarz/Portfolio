948999021164be7f938d31b44a702301bfab0216
plyszczarz <129294476+plyszczarz@users.noreply.github.com>
2023-10-06 17:12:46 +0200

    Add files via upload

---
+++ Wrocław University of Science and Technology /Drone/prj/html/class_pz_g_1_1_info_pliku_do_rysowania.js
@@ -0,0 +1,16 @@
+var class_pz_g_1_1_info_pliku_do_rysowania =
+[
+    [ "InfoPlikuDoRysowania", "class_pz_g_1_1_info_pliku_do_rysowania.html#a42f5a6ba16403a4364158fa46176ff7a", null ],
+    [ "WezEtykiete", "class_pz_g_1_1_info_pliku_do_rysowania.html#ad73ee82ce7a07a63f3229a2add04b93d", null ],
+    [ "WezKolor", "class_pz_g_1_1_info_pliku_do_rysowania.html#a137081c82c29acd7df02badb27c8a2cf", null ],
+    [ "WezNazwePliku", "class_pz_g_1_1_info_pliku_do_rysowania.html#a714aaa4c8e7bbd167ccbb1e797ca158c", null ],
+    [ "WezSposobRys", "class_pz_g_1_1_info_pliku_do_rysowania.html#a04e42126ae7873117b96c39f31e0a206", null ],
+    [ "WezStyl", "class_pz_g_1_1_info_pliku_do_rysowania.html#acf5b136a9ee8a3308fdba974d0eebd9a", null ],
+    [ "WezSzerokosc", "class_pz_g_1_1_info_pliku_do_rysowania.html#a627bb615c50f3b03374774e6b974488b", null ],
+    [ "ZmienEtykiete", "class_pz_g_1_1_info_pliku_do_rysowania.html#a1497c20c07dd13c716ff76d3cc4d7cf3", null ],
+    [ "ZmienKolor", "class_pz_g_1_1_info_pliku_do_rysowania.html#af5c256daaa945a31a1f137c9b429bc37", null ],
+    [ "ZmienNazwePliku", "class_pz_g_1_1_info_pliku_do_rysowania.html#ae734c69f5cecf9c0584e3a7f433340ea", null ],
+    [ "ZmienSposobRys", "class_pz_g_1_1_info_pliku_do_rysowania.html#a4245bff195e0d6d382ed7d776b5ad645", null ],
+    [ "ZmienStyl", "class_pz_g_1_1_info_pliku_do_rysowania.html#a2a7e9e212905112c6f236fe1d0a1f2ec", null ],
+    [ "ZmienSzerokosc", "class_pz_g_1_1_info_pliku_do_rysowania.html#aa6d4c0fa70db640fde80091aefa1326c", null ]
+];
---
+++ Wrocław University of Science and Technology /Drone/prj/html/class_pz_g_1_1_info_punkty_do_rysowania.js
@@ -0,0 +1,15 @@
+var class_pz_g_1_1_info_punkty_do_rysowania =
+[
+    [ "InfoPunktyDoRysowania", "class_pz_g_1_1_info_punkty_do_rysowania.html#aed0725576cd5cf0ac01fc2e528e2c85b", null ],
+    [ "DodajDoEtykiety", "class_pz_g_1_1_info_punkty_do_rysowania.html#a94340d4c219b5469f79cd70cb1638aa3", null ],
+    [ "operator<", "class_pz_g_1_1_info_punkty_do_rysowania.html#acc9f21bca0c987c540783d3bedcbd33a", null ],
+    [ "OtworzAtrybuty", "class_pz_g_1_1_info_punkty_do_rysowania.html#a93c59d4f6fa5687ceef68db7c832c10a", null ],
+    [ "OtworzWspolrz", "class_pz_g_1_1_info_punkty_do_rysowania.html#a9a367a5b7977f3d16f2466a4d5f89a8a", null ],
+    [ "UstawWspolrzEtykietyDlaPrzesuniecia", "class_pz_g_1_1_info_punkty_do_rysowania.html#aee45bb223dcbb35b32934eba41a723c6", null ],
+    [ "WezAtrybuty", "class_pz_g_1_1_info_punkty_do_rysowania.html#a7639b8891f0f7975451b4d675ff7cb31", null ],
+    [ "WezEtykiete", "class_pz_g_1_1_info_punkty_do_rysowania.html#a385228b5ac60713d115d37d267524ec3", null ],
+    [ "WezWspolrz", "class_pz_g_1_1_info_punkty_do_rysowania.html#ae84e4bfa68f132661ff90dac48d83e8c", null ],
+    [ "WezWspolrzEtykiety", "class_pz_g_1_1_info_punkty_do_rysowania.html#a8903b837a2c97c8aede88324d316ac6e", null ],
+    [ "ZmienPrzesEtykiety_WzgRozmPunktu", "class_pz_g_1_1_info_punkty_do_rysowania.html#af2b7bd8684f0b4a4b5465c16e0fe6c71", null ],
+    [ "ZmienPrzesEtykiety_WzgWspoPunktu", "class_pz_g_1_1_info_punkty_do_rysowania.html#aea85f2f3149e375b4c89daad67ccd28e", null ]
+];
